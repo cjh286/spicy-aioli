c5f0d23a35d3b6eb628166d74ba280591c668fc4
Claire Han <cjh286@cornell.edu>
2020-10-21 20:02:15 -0400

    button click fetches data

--- static/js/app.jsx
+++ static/js/app.jsx
@@ -3,20 +3,6 @@ import Button from "./components/button";
 import { getTasks } from "./services/tasks_services";
 
 const App = () => {
-  useEffect(() => {
-    fetch("http://127.0.0.1:5000/getTasks", {
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-      },
-    })
-      .then((response) => {
-        console.log("response: ", response);
-        return response.json();
-      })
-      .then((data) => console.log("data: ", data))
-      .catch((error) => console.log("error: ", error));
-  }, []);
 
   return (
     <>
